663cd8cf21761db4bfa0bdb73ba64c03bd844e03
edwardiscsu <ised03001@stcloudstate.edu>
2015-10-17 14:28:59 -0500

    Added initial MorphablePolygon.setPoints() container. Added common tools for MorphablePoint
    Added MorphablePolygon.setPoints() container, MorphablePoint.distance(),
    & MorphablePoint.unitVector()

--- CG_Morphing/src/MorphablePoint.java
+++ CG_Morphing/src/MorphablePoint.java
@@ -1,5 +1,5 @@
 //class to represent a morphable points
-//for now, let only be able to represent 2 states: stateA & stateB
+//for now, let it only be able to represent 2 states: stateA & stateB
 public class MorphablePoint {
 	public Point2D stateA;
 	public Point2D stateB;
@@ -8,5 +8,27 @@ public class MorphablePoint {
 	public MorphablePoint(Point2D stateA, Point2D stateB) {
 		this.stateA = stateA;
 		this.stateB = stateB;
+	}
+	
+	//get the distance between stateA & stateB
+	public float distance() {
+		float dX = 0f, dY = 0f;
+		if (stateA != null && stateB != null) {
+			dX = stateB.x - stateA.x;
+			dY = stateB.y - stateB.y;
+		}
+		return (float)Math.sqrt(dX * dX + dY * dY);
+	}
+	
+	//get the unit vector between stateA & stateB
+	public Point2D unitVector() {
+		float dX = 0f, dY = 0f;
+		float distance = distance();
+		if (distance != 0 &&
+			stateA != null && stateB != null) {
+			dX = (stateB.x - stateA.x)/distance;
+			dY = (stateB.y - stateB.y)/distance;
+		}
+		return new Point2D(dX, dY);
 	}
 }
--- CG_Morphing/src/MorphablePolygon.java
+++ CG_Morphing/src/MorphablePolygon.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.awt.Polygon.*;
 import java.util.ArrayList;
 
 //class to represent a morphable polygon
@@ -8,5 +10,16 @@ public class MorphablePolygon {
 	public MorphablePolygon() {}
 	public MorphablePolygon(ArrayList<MorphablePoint> points) {
 		this.points = points;
+	}
+	//see setPoints()
+	public MorphablePolygon(Polygon p1, Point2D c1, Polygon p2, Point2D c2) {
+		setPoints(p1, c1, p2, c2);
+	}
+	
+	//set the points by way of two polygons & two center points
+	//p1 & p2 = polygon 1 & 2 respectively
+	//c1 & c2 = center 1 & 2 respectively
+	public void setPoints(Polygon p1, Point2D c1, Polygon p2, Point2D c2) {
+		//todo
 	}
 }
