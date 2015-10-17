f6bf91d4c13dd61106a51bd417e7843d93de67b8
edwardiscsu <ised03001@stcloudstate.edu>
2015-10-17 14:01:03 -0500

    Added the missing center on MorphablePolygon

--- CG_Morphing/src/MorphablePolygon.java
+++ CG_Morphing/src/MorphablePolygon.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 //class to represent a morphable polygon
 public class MorphablePolygon {
 	public ArrayList<MorphablePoint> points;
+	public Point2D center;
 	
 	public MorphablePolygon() {}
 	public MorphablePolygon(ArrayList<MorphablePoint> points) {
