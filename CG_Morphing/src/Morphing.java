0c3835f642807e25a7add1332f42fc70d6e42bc0
Neetu <ntaniraula@gmail.com>
2015-10-18 18:54:29 -0500

    10/18/2015

--- CG_Morphing/src/Morphing.java
+++ CG_Morphing/src/Morphing.java
@@ -22,140 +22,231 @@ public class Morphing extends JFrame {
 
 	public Morphing() {
 		setTitle("Morphing a Polygon");
-		setSize(400, 400);
-		add("Center", new cvMorphing());
+		setSize(800, 400);
+		add("Center", new CvMorphing());
 
 	}
 }
 
-class cvMorphing extends Canvas implements MouseListener {
-	Point DCenter;
-	Point Scenter ;
-	Polygon dest_polygon = new Polygon();
-	Polygon src_polygon = new Polygon();
-	boolean DestPolyClosed ;
-	boolean SrcPolyClosed;
+class CvMorphing extends Canvas implements MouseListener {
+	Point tCenter;
+	Point refCenter;
+	Polygon targetPolygon = new Polygon();
+	Polygon refPolygon = new Polygon();
+	boolean targetPolyClosed;
+	boolean refPolyClosed;
+	boolean MouseClicked;
 	String msg;
-	
+	double distanceRefrence; // calculating the distance between polygon
+								// starting vertex to center
 
-	public cvMorphing() {
+	Point2D dirCP = new Point2D(); // point to find the vector for the reference
+									// line
+	Point hRefLinePoint, vRefLinePoint;
+
+	public CvMorphing() {
 		this.addMouseListener(this);
-		
 
 	}
 
+	// paint method to draw polygons
 	public void paint(Graphics g) {
 		super.paint(g);
 		Dimension d = getSize();
-		if(Scenter != null ){
- 				g.fillArc(Scenter.x-2, Scenter.y-2, 4, 4,0,360);
- 				msg = "Center(" +Scenter.x+ ","+Scenter.y+ ")";
- 				g.drawString(msg,Scenter.x-20, Scenter.y-20);
+		
+		if(!MouseClicked)
+		{
+				msg = "Please Click to start";
+				g.drawString(msg, (d.width - 100)/2, (d.height-100)/2);
 		}
-		if(DCenter != null){
-	 		 	g.fillArc(DCenter.x-2, DCenter.y-2, 4, 4,0,360);
-	 	}
-	 	if(SrcPolyClosed == true){
- 				draw_DestPoly_points(dest_polygon,g);
- 		}
-	 			draw_SrcPoly_points(src_polygon,g);
+		else
+		{
+				msg = "In order to draw polygon you have to always move in Clockwise Direction";
+				g.drawString(msg, 20, 20);
+		}
+		drawPolygon(targetPolygon, g, Color.BLUE, targetPolyClosed);
+		drawPolygon(refPolygon, g, Color.MAGENTA, refPolyClosed);
 
+		// first click on the frame
+		if (refCenter != null) {
+			g.fillArc(refCenter.x - 2, refCenter.y - 2, 4, 4, 0, 360);
+			msg = "Center(" + refCenter.x + "," + refCenter.y + ")";
+			g.drawString(msg, refCenter.x - 20, refCenter.y - 20);
+		}
+		// The first click after our reference polygon is drawn to draw another polygon
+		if (tCenter != null) {
+			g.fillArc(tCenter.x - 2, tCenter.y - 2, 4, 4, 0, 360);
+		}
+
+		
+		drawReferenceLines(g);
 	}
 
 	// Draw different points on the polygon on every mouse click
-	private void draw_SrcPoly_points(Polygon sp, Graphics g) {
-		int SrcPoly_points = sp.npoints;
-		double distanceRefrence;
-		Point2D SP = new Point2D();
-		Point Ref = new Point();
-		
-		for (int i = 0; i < SrcPoly_points; i++) {
-			g.fillRect(sp.xpoints[i] - 2, sp.ypoints[i] - 2, 4, 4);
-			if (i == 0)
-				g.drawArc(sp.xpoints[i] - 5, sp.ypoints[i] - 5, 10, 10, 0, 360);
-			if (i > 0)
-				g.drawLine(sp.xpoints[i - 1], sp.ypoints[i - 1], sp.xpoints[i], sp.ypoints[i]);
+	private void drawPolygon(Polygon p, Graphics g, Color color, boolean closePolygon) {
+		int npoints = p.npoints; // number of points in polygon
+		Color orgColor = g.getColor();
+		g.setColor(color);
+		if (closePolygon == false) {
+			// This handles the condition for drawing a source polygon after we
+			// have initialized the center for our polygon.
+			for (int i = 0; i < npoints; i++) {
 
-			
-			//After the polygon gets closed draw a reference line from the center.
-			if(SrcPolyClosed == true )
-			{
-				distanceRefrence = Math.sqrt(((src_polygon.xpoints[0]-Scenter.x)*(src_polygon.xpoints[0]-Scenter.y)) +((src_polygon.ypoints[0]-Scenter.y)*(src_polygon.ypoints[0]-Scenter.y)));
-				//unit vector of reference line
-				SP.x =(float)(((src_polygon.xpoints[0]-Scenter.x)/distanceRefrence));
-				SP.y = (float)(((src_polygon.ypoints[0]-Scenter.y))/distanceRefrence);
-				Ref.x = (int)(src_polygon.xpoints[0] + distanceRefrence * SP.x);
-				Ref.y = (int)(src_polygon.ypoints[0] + distanceRefrence * SP.y);
-				g.drawLine(Scenter.x,Scenter.y,Ref.x,Ref.y);
-					
-				
+				g.fillRect(p.xpoints[i] - 2, p.ypoints[i] - 2, 4, 4);
+
+				// for the starting point of the polygon mark it with arc
+				if (i == 0) {
+					g.drawArc(p.xpoints[i] - 5, p.ypoints[i] - 5, 10, 10, 0, 360);
+
+				}
+				//start connecting the points of polygon
+				if (i > 0)
+
+					g.drawLine(p.xpoints[i - 1], p.ypoints[i - 1], p.xpoints[i], p.ypoints[i]);
+
 			}
+		}
+		else
+		{
+			g.fillPolygon(p);  //after polygon is closed, fill the shape with the desired color
+		}
+		g.setColor(orgColor);
+	}
 
+	
+	//This handles drawing reference lines on the polygon with respect to the polygon point and Center
+	//Two reference line were drawn one in the direction of Center and first polygon point
+	// Another in the perpendicular direction of the reference polygon first point.
+	private void drawReferenceLines(Graphics g) {
+		int npoints;
+
+		if (refCenter != null) {
+			if (refPolygon.npoints > 0) {
+				npoints = refPolygon.npoints - 1;
+				// After the polygon gets closed draw a reference line from the
+				// center.
+				
+				// This calculates the distance bewteen the center of the reference polygon and its point.
+				distanceRefrence = Math.sqrt(((refPolygon.xpoints[npoints] - refCenter.x)
+						* (refPolygon.xpoints[npoints] - refCenter.y))
+						+ ((refPolygon.ypoints[npoints] - refCenter.y) * (refPolygon.ypoints[npoints] - refCenter.y)));
+
+				// unit vector of reference line
+				vRefLinePoint = new Point();
+				hRefLinePoint = new Point();
+				
+				//direction  of line from center of reference polygon to the first polygon point
+				dirCP.x = (float) (((refPolygon.xpoints[npoints] - refCenter.x) / distanceRefrence));
+				dirCP.y = (float) (((refPolygon.ypoints[npoints] - refCenter.y) / distanceRefrence));
+
+				// To draw horizontal and vertical refrence line points with respect to the polygon vertex
+				vRefLinePoint.x = Math.round((float) (refPolygon.xpoints[npoints] + distanceRefrence * dirCP.x));
+				vRefLinePoint.y = Math.round((float) (refPolygon.ypoints[npoints] + distanceRefrence * dirCP.y));
+
+				hRefLinePoint.x = Math.round((float) (refPolygon.xpoints[npoints] + distanceRefrence * dirCP.y));
+				hRefLinePoint.y = Math.round((float) (refPolygon.ypoints[npoints] - distanceRefrence * dirCP.x));
+
+				// Draw a reference line from the  Center to  reference polygon
+				g.setColor(Color.red);
+				g.drawLine(refCenter.x, refCenter.y, vRefLinePoint.x, vRefLinePoint.y);
+				g.drawLine(hRefLinePoint.x, hRefLinePoint.y, refPolygon.xpoints[npoints], refPolygon.ypoints[npoints]);
+
+			}
 		}
 	}
-		private void draw_DestPoly_points(Polygon dp, Graphics g) {
-			int DestPoly_points = dp.npoints;
+
+	//This condition checks weather the clicked point is valid to form a star shaped polygon
+	
+	private boolean isPointValid(Polygon p, Point c, Point t) {
+		int npoints = p.npoints-1;
+		boolean result;
+		Point2D X = new Point2D();
+		Point2D dir = new Point2D();
+		float dist;
+
+		if (p.npoints == 0)
+			return true;
+		else {
 			
-			for(int i = 0; i < DestPoly_points; i++)
-			{
-				g.fillArc(dp.xpoints[i]-2, dp.ypoints[i]-2, 4, 4, 0, 360);
-				if(i==0)
-					g.drawArc(dp.xpoints[0]-5,dp.ypoints[0]-5, 10, 10, 0, 360);
-				else
-				{
-					g.drawLine(dp.xpoints[i-1], dp.ypoints[i-1], dp.xpoints[i],dp.ypoints[i]);
+			dist = (float) Math.sqrt(((p.xpoints[npoints] - c.x)
+					* (p.xpoints[npoints] - c.y))
+					+ ((p.ypoints[npoints] - c.y) * (p.ypoints[npoints] - c.y)));
+
+			
+			dir.x = (float) (((p.xpoints[npoints] - c.x) / dist));
+			dir.y = (float) (((p.ypoints[npoints] - c.y) / dist));
+			
+			float d = (t.x - c.x) * (c.y - p.ypoints[npoints]) - (c.y - t.y) * (p.xpoints[npoints] - c.x);
+			// float PerpDistance2 =
+			if (d > 0)
+				result = true;
+			else
+				result = false;
+
+			if (result == true) {
+
+				X.x = (float) (p.xpoints[npoints] + (100 * dir.y));
+				X.y = (float) (p.ypoints[npoints] - (100 * dir.x));
+
+				d = (t.x - X.x) * (X.y - p.ypoints[npoints]) - (X.y - t.y) * (p.xpoints[npoints] - X.x);
+
+				if ((npoints) % 2 == 0) {
+					if ((d > 0) || (d == 0))
+						result = false;
+					else if (d < 0)
+						result = true;
+				} else {
+					if ((d < 0) || (d == 0))
+						result = false;
+					else if (d > 0)
+						result = true;
 				}
 			}
-			
-		}	
-		
+			return result;
+		}
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent event) {
-		if(Scenter == null)
-		{
-				Scenter = new Point(event.getX(),event.getY());
+		
+		MouseClicked = true; 
+		if (refCenter == null) {
+			refCenter = new Point(event.getX(), event.getY());
 		}
-	
-		else if(!SrcPolyClosed)
-		{
-			if((event.getX() >= src_polygon.xpoints[0]-5) && (event.getY() >= src_polygon.ypoints[0]-5) && (event.getX() <= src_polygon.xpoints[0]+5) &&
-				(event.getY() <= src_polygon.ypoints[0]+5))
-					{
-						src_polygon.addPoint(src_polygon.xpoints[0], src_polygon.ypoints[0]);
-						SrcPolyClosed =true;
-					}
-					else
-					{
-						src_polygon.addPoint(event.getX(),event.getY());
-					}
-					
-					
+
+		else if (!refPolyClosed) {
+			if (isPointValid(refPolygon, refCenter, new Point(event.getX(), event.getY()))) {
+				if ((event.getX() >= refPolygon.xpoints[0] - 5) && (event.getY() >= refPolygon.ypoints[0] - 5)
+						&& (event.getX() <= refPolygon.xpoints[0] + 5) && (event.getY() <= refPolygon.ypoints[0] + 5)) {
+					refPolygon.addPoint(refPolygon.xpoints[0], refPolygon.ypoints[0]);
+					refPolyClosed = true;
+				} else {
+					refPolygon.addPoint(event.getX(), event.getY());
+				}
+			}
+
 		}
-	
-		else if(SrcPolyClosed = true &&!DestPolyClosed){
-			
-				if(DCenter == null)
-					{
-						DCenter = new Point(event.getX(),event.getY());
-					}
-				else {
-	
-			if((event.getX() >= dest_polygon.xpoints[0]-5) && (event.getY() >= dest_polygon.ypoints[0]-5) && (event.getX() <= dest_polygon.xpoints[0]+5) &&
-				(event.getY() <= dest_polygon.ypoints[0]+5))
-						{
-							dest_polygon.addPoint(dest_polygon.xpoints[0], dest_polygon.ypoints[0]);
-							DestPolyClosed =true;
-						}
-				else
-					{
-							dest_polygon.addPoint(event.getX(),event.getY());
+
+		else if (refPolyClosed == true && !targetPolyClosed) {
+
+			if (tCenter == null) {
+				tCenter = new Point(event.getX(), event.getY());
+			} else {
+				if (isPointValid(targetPolygon, tCenter, new Point(event.getX(), event.getY()))) {
+
+					if ((event.getX() >= targetPolygon.xpoints[0] - 5) && (event.getY() >= targetPolygon.ypoints[0] - 5)
+							&& (event.getX() <= targetPolygon.xpoints[0] + 5)
+							&& (event.getY() <= targetPolygon.ypoints[0] + 5)) {
+						targetPolygon.addPoint(targetPolygon.xpoints[0], targetPolygon.ypoints[0]);
+						targetPolyClosed = true;
+					} else {
+						targetPolygon.addPoint(event.getX(), event.getY());
 					}
 				}
+			}
 		}
-		
+
 		repaint();
-	
 
 	}
 
