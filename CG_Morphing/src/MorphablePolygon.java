5262a8dcc1a2ed06d0d1b1ee3864072d5ea4e882
edwardiscsu <ised03001@stcloudstate.edu>
2015-10-18 23:41:13 -0500

    Added temporary logic to MorphablePolygon.setPoints()
    Added temporary logic to generate points from polygons pA, pB, and
    center cA & cB.

--- CG_Morphing/src/MorphablePolygon.java
+++ CG_Morphing/src/MorphablePolygon.java
@@ -20,6 +20,30 @@ public class MorphablePolygon {
 	//pA & pB = polygon A & B respectively
 	//cA & cB = center of polygon A & B respectively
 	public void setPoints(Polygon pA, Point2D cA, Polygon pB, Point2D cB) {
-		//todo
+		center = new Point2D(cA.x, cA.y); //set pA's center as the polygon's center
+		float xDiff = cB.x - cA.x;
+		float yDiff = cB.y - cA.y;
+	
+		//set the first element of pA as reference point
+		Point2D basePoint = new Point2D(pA.xpoints[0], pA.ypoints[0]);
+		
+		//TODO!
+		//temporary logic
+		for (int i = 0; i < pA.xpoints.length; i++) {
+			points.add(new MorphablePoint(
+				new Point2D(pA.xpoints[i], pA.ypoints[i]),
+				new Point2D(0, 0)));
+		}
+		for (int i = 0; i < pB.xpoints.length; i++) {
+			if (i >= pA.xpoints.length)
+				points.add(new MorphablePoint(
+					new Point2D(pB.xpoints[i], pB.ypoints[i]),
+					new Point2D(pB.xpoints[i], pB.ypoints[i])));
+			else
+				points.add(i, new MorphablePoint(
+					new Point2D(pA.xpoints[i], pA.ypoints[i]),
+					new Point2D(pB.xpoints[i], pB.ypoints[i])));
+		}
+		//temporary logic - END
 	}
 }
